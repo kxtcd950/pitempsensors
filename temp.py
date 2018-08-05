b7f537d6b5b43d1445ead41f8973fee7ab8218cf
Dave Jolley <github@lucien.me.uk>
2018-08-05 19:14:14 +0000

    If the script doesn't connect to MQTT, it now displays on stdout.

--- temp.py
+++ temp.py
@@ -7,31 +7,55 @@ import paho.mqtt.client as mqtt
 from time import sleep
 import json
 
-with open('config.json') as config:
-    conf = json.load(config)
-
+if (os.path.isfile('config.json') == True):
+    with open('config.json') as config:
+        conf = json.load(config)
+connected = False;
 # add defaults for sensor location and the pattern we use to discover them.
 # This simplifies the config for people who've not messed about with udev
 # but means the script can work for those which do.
-if 'sensordir' in conf:
-    sensordir=conf['sensordir']
-else:
+try:
+    if 'sensordir' in conf:
+        sensordir=conf['sensordir']
+    else:
+        sensordir='/sys/bus/w1/devices/'
+except:
     sensordir='/sys/bus/w1/devices/'
-if 'sensorregex' in conf:
-    sensorregex=r''+conf["sensorregex"]
-else:
+try:
+    if 'sensorregex' in conf:
+        sensorregex=r''+conf["sensorregex"]
+    else:
+        sensorregex=r'[0-9a-f]{2}-[0-9a-f]{12}'
+except:
     sensorregex=r'[0-9a-f]{2}-[0-9a-f]{12}'
-sensornames=conf['sensornames']
-mqttsrv=conf['mqttsrv']
-mqttuser=conf['mqttuser']
-mqttpass=conf['mqttpass']
-mqtttopic=conf['mqtttopic']
+try:
+    sensornames=conf['sensornames']
+except:
+    sensornames=[]
+try:
+    mqttsrv=conf['mqttsrv']
+except:
+    mqttsrv="127.0.0.1"
+try:
+    mqttuser=conf['mqttuser']
+except:
+    mqttuser=""
+try:
+    mqttpass=conf['mqttpass']
+except:
+    mqttpass=""
+try:
+    mqtttopic=conf['mqtttopic']
+except:
+    mqtttopic="tempsense/"
 
 if (mqtttopic[-1:] != '/'):
     mqtttopic += '/'
 
 def on_connect(client,userdata,flags,rc):
+    global connected
     print("Connected to mqtt server!")
+    connected=True;
     return
 
 def get_device_list():
@@ -61,7 +85,11 @@ def get_temperature(dev_file):
 client = mqtt.Client()
 client.on_connect=on_connect
 client.username_pw_set(mqttuser, mqttpass)
-client.connect(mqttsrv, 8883, 60)
+try:
+    client.connect(mqttsrv, 8883, 60)
+except:
+    print("Cannot connect to MQTT server")
+
 devices = get_device_list()
 if (len(devices) == 0):
     print("Didn't find any devices.\n")
@@ -79,7 +107,10 @@ while (1):
         thistemp = get_temperature(device[1])
         repval = round(thistemp[0] * 2) / 2
         if (repval != device[2]):
-            client.publish(mqtttopic+sensornames[device[0]]+"/temperature", str(repval))
+            if (connected == True):
+                client.publish(mqtttopic+sensornames[device[0]]+"/temperature", str(repval))
+            else:
+                print("Temperature reading: "+str(repval))
             device[2] = repval
     sleep(10)
 
